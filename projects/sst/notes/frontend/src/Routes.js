4d4f90730348118c28ccde555e68eec211d00786
Your Name <mahmoud.m.y234@gmail.com>
2022-09-28 13:25:36 +0200

    Some react sst

--- projects/sst/notes/frontend/src/Routes.js
+++ projects/sst/notes/frontend/src/Routes.js
@@ -1,12 +1,18 @@
+import Signup from "./containers/Signup";
+import NewNote from "./containers/NewNote";
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
+import Login from "./containers/Login";
 
 export default function Links() {
 	return (
 		<Routes>
 			<Route path="/" element={<Home />} />
+			<Route path="/login" element={<Login />} />
+			<Route path="/signup" element={<Signup />} />
+			<Route path="/notes/new" element={<NewNote />} />
 			{
 				/* Finally, catch all unmatched routes */
 			}
